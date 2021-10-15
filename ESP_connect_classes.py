f7b4faad5a4c05275b492611dbe05045f3efa70e
Garvet <Mitriel-leirtim@mail.ru>
2021-10-15 16:27:14 +0300

    Edit search function

--- ESP_connect_classes.py
+++ ESP_connect_classes.py
@@ -3,17 +3,21 @@
 Классы для контроля общения между модулем управления группой и сервером.
 
 В данном модуле описаны следующие классы пакетов:
-- C{Packet} : Общий класс пакетов;
-- C{SystemRegistration} : Класс пакета завершения регистрации;
-- C{Module} : Класс пакета отчётов компонентов;
-- C{SystemControl} : Класс пакета состояний модуля;
-- C{Error} : Класс пакета ошибок;
-- C{Check} : Класс пакета проверки наличия команд;
-- C{SystemSetting} : Класс пакета настройки системы.
+(+) 0xFF - C{Packet} : Общий класс пакетов;
+(+) 0x00 - C{SystemRegistration} : Класс пакета завершения регистрации;
+(+) 0x01 - C{Module} : Класс пакета отчётов компонентов;
+(+) 0x02 - C{SystemControl} : Класс пакета состояний модуля;
+(+) 0x03 - C{TimeControl} : Класс пакета контроля времени;
+(+) 0x04 - C{Error} : Класс пакета ошибок;
+(+) 0x05 - C{Check} : Класс пакета проверки наличия команд;
+(+) 0x06 - C{SystemSetting} : Класс пакета настройки системы;
+(-) 0x07 - C{ACS} : Класс пакетов связанных с системой контроля доступа.
 """
 # В файле есть (-) -----
 import ESP_connect_interface_data as PEnum
 import ESP_connect_bytelist as PByte
+
+import datetime
 
 
 # Create send packet (ByteList->header|data|CRC) (-) ----- (+/-) data -> CRC in ESP_connect_bytelist
@@ -47,55 +51,58 @@ def check_id(string_id: str):
     return False
 
 
-def get_PT_num(search_value):
+def get_PT_num(search_value, search_area='Packet'):
     """ Получение номера типа пакета
 
     @param search_value : С{str} название типа пакета.
                           С{type(Packet)} тип пакета.
+    @param search_area : C{Dict} область поиска типа
     @return : C{int} номер типа пакета.
               C{None} ошибка поиска.
     """
-    if search_value in [value for key, values in packet_type.items() for value in values.values()]:
+    if search_value in [value for key, values in packet_type[search_area].items() for value in values.values()]:
         if isinstance(search_value, str):
-            return [key for key, value in packet_type.items() if value['name'] == search_value][0]
+            return [key for key, value in packet_type[search_area].items() if value['name'] == search_value][0]
         elif issubclass(search_value, Packet):
-            return [key for key, value in packet_type.items() if value['class'] == search_value][0]
+            return [key for key, value in packet_type[search_area].items() if value['class'] == search_value][0]
     return None
 
 
-def get_PT_name(search_value):
+def get_PT_name(search_value, search_area='Packet'):
     """ Получение названия типа пакета
 
     @param search_value : С{int} номер типа пакета.
                           С{type(Packet)} тип пакета.
+    @param search_area : C{Dict} область поиска типа
     @return : C{str} название типа пакета.
               C{None} ошибка поиска.
     """
     if isinstance(search_value, int):
-        if search_value not in packet_type:
+        if search_value not in packet_type[search_area]:
             return None
-        return packet_type[search_value]['name']
+        return packet_type[search_area][search_value]['name']
     elif issubclass(search_value, Packet):
-        if search_value in [value for key, values in packet_type.items() for value in values.values()]:
-            return packet_type[get_PT_num(search_value)]['name']
+        if search_value in [value for key, values in packet_type[search_area].items() for value in values.values()]:
+            return packet_type[search_area][get_PT_num(search_value)]['name']
     return None
 
 
-def get_PT(search_value):
+def get_PT(search_value, search_area='Packet'):
     """ Получение типа пакета
 
     @param search_value : С{int} номер типа пакета.
                           С{str} название типа пакета.
+    @param search_area : C{Dict} область поиска типа
     @return : C{type(Packet)} тип пакета.
               C{None} ошибка поиска.
     """
     if isinstance(search_value, int):
-        if search_value not in packet_type:
+        if search_value not in packet_type[search_area]:
             return None
-        return packet_type[search_value]['class']
+        return packet_type[search_area][search_value]['class']
     elif isinstance(search_value, str):
-        if search_value in [value for key, values in packet_type.items() for value in values.values()]:
-            return packet_type[get_PT_num(search_value)]['class']
+        if search_value in [value for key, values in packet_type[search_area].items() for value in values.values()]:
+            return packet_type[search_area][get_PT_num(search_value)]['class']
     return None
 
 
@@ -455,6 +462,75 @@ class SystemControl(Packet):
     # Packet->ByteList (-) -----
 
 
+# ByteList->Packet (-) -----
+# Packet->ByteList (-) -----
+class TimeControl(Packet):
+    """Пакет управления временем модуля
+
+    Пакет управления временем в системе узлов. Содержит в себе поле с самим ID
+    и поля даты и времени.
+    """
+    type_name = 'TimeControl'
+
+    def __init__(self, module_id: str, state: datetime.datetime,
+                 packet=None, group_id: str = None, time: str = None):
+        """Конструктор пакета состояний модуля
+
+        @param module_id : C{str} - ID компонента, состоящее из 12 символов в шестнадцатиричном виде.
+        @param state : C{str} - Состояние компонента [см. state_system].
+        @param packet : C{SystemControl}/C{dict} - см. Packet.
+        @param group_id : C{str} - см. Packet.
+        @param time : C{str} - см. Packet.
+        """
+        if isinstance(packet, SystemControl) or isinstance(packet, dict):
+            super().__init__(packet=packet)
+        else:
+            super().__init__(group_id=group_id, time=time)
+        self.class_error[SystemControl.type_name] = []
+        self.type = get_PT_num(SystemControl.type_name)
+        self.module_id = ''
+        self.state = ''
+        # Конструктор по значению
+        if isinstance(module_id, str) and isinstance(state, str):
+            if state in PEnum.state_system.values():
+                self.module_id = module_id
+                self.state = state
+            else:
+                self.class_error[SystemControl.type_name].append('__init__: Invalid state system.')
+        # Конструктор копирования
+        elif isinstance(packet, SystemControl):
+            self.module_id = packet.module_id
+            self.state = packet.state
+        # Конструктор из словаря
+        elif isinstance(packet, dict) and packet.keys() >= {'module_id', 'state'}:
+            self.module_id = packet['module_id']
+            self.state = packet['state']
+        # Ошибка конструирования
+        else:
+            self.class_error[SystemControl.type_name].append('__init__: Invalid parameters type.')
+
+    def get_class_error(self, one_mas: bool = False):
+        """Возврат ошибок класса
+
+        @param one_mas : C{bool} - см. Packet.
+        @return: см. Packet.
+        """
+        if isinstance(one_mas, bool):
+            if one_mas:
+                return super().get_class_error(one_mas) + self.class_error[SystemControl.type_name]
+            else:
+                return self.class_error
+        else:
+            self.class_error[SystemControl.type_name].append('get_class_error: Invalid parameters type.')
+            return
+
+    def get_packet_len(self):
+        return super().get_packet_len() + 13
+
+    # ByteList->Packet (-) -----
+    # Packet->ByteList (-) -----
+
+
 # get_packet_len (-) -----
 # ByteList->Packet (-) -----
 # Packet->ByteList (-) -----
@@ -664,13 +740,25 @@ class SystemSetting(Packet):
     # Packet->ByteList (-) -----
 
 
-packet_type = {0xFF: {'name': Packet.type_name,             'class': Packet},
-               0x00: {'name': SystemRegistration.type_name, 'class': SystemRegistration},
-               0x01: {'name': Module.type_name,             'class': Module},
-               0x02: {'name': SystemControl.type_name,      'class': SystemControl},
-               0x03: {'name': Error.type_name,              'class': Error},
-               0x04: {'name': Check.type_name,              'class': Check},
-               0x05: {'name': SystemSetting.type_name,      'class': SystemSetting}}
+# packet_type = {0xFF: {'name': Packet.type_name,             'class': Packet},
+#                0x00: {'name': SystemRegistration.type_name, 'class': SystemRegistration},
+#                0x01: {'name': Module.type_name,             'class': Module},
+#                0x02: {'name': SystemControl.type_name,      'class': SystemControl},
+#                0x03: {'name': Error.type_name,              'class': Error},
+#                0x04: {'name': Check.type_name,              'class': Check},
+#                0x05: {'name': SystemSetting.type_name,      'class': SystemSetting}}
+
+packet_type = dict()
+packet_type['Packet'] = {0xFF: {'name': Packet.type_name,             'class': Packet},
+                         0x00: {'name': SystemRegistration.type_name, 'class': SystemRegistration},
+                         0x01: {'name': Module.type_name,             'class': Module},
+                         0x02: {'name': SystemControl.type_name,      'class': SystemControl},
+                         0x03: {'name': TimeControl.type_name,        'class': TimeControl},
+                         0x04: {'name': Error.type_name,              'class': Error},
+                         0x05: {'name': Check.type_name,              'class': Check},
+                         0x06: {'name': SystemSetting.type_name,      'class': SystemSetting}
+                         # 0x07: {'name': ACS.type_name,                'class': ACS}
+                         }
 
 
 #
